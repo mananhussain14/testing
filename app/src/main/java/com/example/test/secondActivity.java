5bdd47b76f7d1387447a10c661b459cc22e75ade
Manan Hussain <ma.hussain@wasl.ae>
2022-12-02 12:39:04 +0400

    3rd commit

--- app/src/main/java/com/example/test/secondActivity.java
+++ app/src/main/java/com/example/test/secondActivity.java
@@ -3,33 +3,120 @@ package com.example.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class secondActivity extends AppCompatActivity {
     private static final String TAG = "secondActivity";
+    Handler handler;
+    Boolean stopThread;
+    Button buttonStartThread;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-    }
+        buttonStartThread = findViewById(R.id.startThread);
+        handler = new Handler();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
 
+            }
+        });
+
+    }
     public void onStartClicked(View view) {
-        myThread myThread = new myThread();
-        myThread.start();
+        stopThread = false;
+        ExampleRunnable runnable = new ExampleRunnable(10);
+        new Thread(runnable).start();
+        /*
+        ExampleThread thread = new ExampleThread(10);
+        thread.start();
+        */
+        /*
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //work
+            }
+        }).start();
+        */
     }
 
     public void onStopclicked(View view) {
+        stopThread = true;
     }
 
-    public class myThread extends Thread{
+    class ExampleThread extends Thread {
+        int seconds;
+
+        ExampleThread(int seconds) {
+            this.seconds = seconds;
+        }
+
         @Override
         public void run() {
-            Log.d(TAG, "run: "+ this.getName());
-            Log.d(TAG, "run: "+ this.getName());
+            for (int i = 0; i < seconds; i++) {
+                Log.d(TAG, "startThread: " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    class ExampleRunnable implements Runnable {
+        int seconds;
+
+        ExampleRunnable(int seconds) {
+            this.seconds = seconds;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < seconds; i++) {
+                if (stopThread)
+                    return;
+                if (i == 5) {
+                    /*
+                    Handler threadHandler = new Handler(Looper.getMainLooper());
+                    threadHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            buttonStartThread.setText("50%");
+                        }
+                    });
+                    */
+                    /*
+                    buttonStartThread.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            buttonStartThread.setText("50%");
+                        }
+                    });
+                    */
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            buttonStartThread.setText("50%");
+                        }
+                    });
+                }
+                Log.d(TAG, "startThread: " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
